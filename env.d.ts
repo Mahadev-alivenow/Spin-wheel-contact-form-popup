531913dfe4a8a9392e2342a8e63625352cb42fcc
Mahadev-Maddy <mahadev.c@alivenow.in>
2024-08-08 01:19:11 +0530

    remix host app url added

--- env.d.ts
+++ env.d.ts
@@ -1,4 +1,4 @@
 /// <reference types="vite/client" />
 /// <reference types="@remix-run/node" />
-process.env.HOST="https://seahorse-app-fstfy.ondigitalocean.app/";
+process.env.SHOPIFY_APP_URL="https://seahorse-app-fstfy.ondigitalocean.app/";
 
