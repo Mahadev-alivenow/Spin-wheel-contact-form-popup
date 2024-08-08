d5cfee13e9eefe1f2d7dc9ff02ee5a2f36662a5b
Mahadev-Maddy <mahadev.c@alivenow.in>
2024-08-08 13:12:29 +0530

    shop url

--- app/shopify.server.js
+++ app/shopify.server.js
@@ -13,9 +13,7 @@ const shopify = shopifyApp({
   apiSecretKey: process.env.SHOPIFY_API_SECRET || "",
   apiVersion: ApiVersion.July24,
   scopes: process.env.SCOPES?.split(","),
-  appUrl:
-    process.env.SHOPIFY_APP_URL ||
-    "https://seahorse-app-fstfy.ondigitalocean.app/",
+  appUrl: process.env.SHOPIFY_APP_URL || "",
   authPathPrefix: "/auth",
   sessionStorage: new PrismaSessionStorage(prisma),
   distribution: AppDistribution.AppStore,
--- env.d.ts
+++ env.d.ts
@@ -1,4 +1,4 @@
 /// <reference types="vite/client" />
 /// <reference types="@remix-run/node" />
-process.env.SHOPIFY_APP_URL="https://seahorse-app-fstfy.ondigitalocean.app/";
+SHOPIFY_APP_URL="https://seahorse-app-fstfy.ondigitalocean.app/";
 
